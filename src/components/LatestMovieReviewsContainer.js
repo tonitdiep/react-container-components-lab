6346e826542a78cea802d6cdef1a445f9393ef28
tonitdiep <adiep123@gmail.com>
2021-05-22 23:56:49 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,33 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+    constructor() {
+        super();
+        this.state = {
+            reviews: []
+        };
+    }
+
+
+    componentDidMount() {
+        console.log('inside component DID MOUNT')
+        fetch(URL)
+            .then(res => res.json())
+            .then(response => this.setState({ reviews: response.results }) )
+
+    }
+
+    render() {
+        // console.log("inside render")
+        return(
+            <div className="latest-movie-reviews">
+            <h1>Latest Reviews</h1>
+            
+            <MovieReviews reviews={this.state.reviews}/>
+        </div>
+           ); 
+    }
+}
+
+export default LatestMovieReviewsContainer;
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,38 @@
 // Code MovieReviews Here
+import React from 'react'
+const Review = ({
+    headline,
+    byline,
+    link,
+    summary_short
+}) => {
+    return(
+        <div 
+        className="review">
+        <header>HEADER: Headline<br/>
+            <a className="review-link" href={link.url} >{headline}</a><br/>
+            <span className="author">Reviewed By: {byline}</span>
+            {/* SUMMARY SHORT: {summary_short} */}
+            {/* <blockquote>{summary_short}</blockquote> */}
+      </header>
+        <blockquote>SUMMARY DESCRIPTION: {summary_short}</blockquote>
+        </div>
+    )
+}
+
+const MovieReviews = ({ reviews }) =>  <div className="review-list">{reviews.map(Review)}</div>;
+    // console.log(props)
+    // (headline,
+    // byline,
+    // link
+    // summary_short)
+    // return (
+
+    //     <div className="review-list">
+    //         title: {props.title}
+    //     </div>
+    // )
+    MovieReviews.defaultProps = {
+        reviews: []
+    };
+export default MovieReviews
